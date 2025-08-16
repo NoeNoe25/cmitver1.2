28c6bf5eeab78c8404f6d7f003cb8200af516394
Hsu Myat Noe <hsumyatnoe031@gmail.com>
2025-08-17 02:12:04 +0700

    Update App.js

--- siitec/src/App.js
+++ siitec/src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
 import React from 'react';
 import Header from './components/header';
-import Hero from './components/hero';
 import Faculty from './components/faculty';
 import Footer from './components/footer';
 import './App.css';
