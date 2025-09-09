8ea33eb3833d2f641431e5543b66ffa710ffd00e
Hsu Myat Noe <hsumyatnoe031@gmail.com>
2025-09-10 01:58:22 +0700

    adding albums and fixing the background-color of banner

--- siitec/src/App.js
+++ siitec/src/App.js
@@ -14,9 +14,9 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <PartnersBanner/>
         <Departments />
         <Faculty />
+        <PartnersBanner/>
       </main>
       <Footer />
     </div>
